e593af1849c46b1d4327e81395257349b48176b4
Nikitaprobuz9 <78540710+Nikitaprobuz9@users.noreply.github.com>
2021-02-09 22:20:32 +0530

    volume of a sphere

---
+++ Volume of a Sphere.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+void sphere();
+void sphere() 
+{
+	float r,Volume_of_Sphere;
+	cout << "Enter the radius of sphere " << endl;
+	cin>>r;
+	Volume_of_Sphere=1.33*3.14*r*r*r;
+	cout<<"Volume_of_Sphere is : "<<Volume_of_Sphere<<endl;
+
+}
+int main() 
+{
+	sphere();
+}
