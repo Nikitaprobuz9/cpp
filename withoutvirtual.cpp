e1919cf37ace6fb0fe277edee4f5434d22287672
Nikitaprobuz9 <78540710+Nikitaprobuz9@users.noreply.github.com>
2021-02-11 21:51:57 +0530

    WAP of Without virtual function

---
+++ withoutvirtual.cpp
@@ -0,0 +1,32 @@
+/*With virtual function*/
+#include<iostream>
+#include<conio.h>
+using namespace std;
+class Employee
+{
+	public:
+		void dis()
+		{
+			cout<<"Mai Employee huuu....."<<endl;
+		}
+		
+};
+class Trainer: public Employee
+{
+	public:
+		void dis()
+		{
+			cout<<"Mai Trainer huuu...."<<endl;
+		}
+};
+int main()
+{
+	Employee e1,*e1pt;
+	e1pt=&e1;
+	e1pt->dis();
+	
+	Trainer t1;
+	e1pt=&t1;
+	e1pt->dis();
+	
+}
